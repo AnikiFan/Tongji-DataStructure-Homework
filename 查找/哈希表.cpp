9abc0d0958b65385d8b4f6109165fbe1a9ed58b2
AnikiFan <15800788661@163.com>
2024-01-05 23:05:56 +0800

    update

--- 查找/哈希表.cpp
+++ 查找/哈希表.cpp
@@ -2,60 +2,49 @@
 typedef int Status;
 #define OK 1
 using namespace std;
-unsigned long long get_max_prime(unsigned long long n)
+bool is_prime(unsigned long long n)
 {
-	if(n%2)
-	  n --;
-	int flag = 1;
-	while(n>0){
-		flag= 1;
-		int i = n -1;
-		while(i>1){
-			if(n%i){
-				flag =0;
-				break;
-			}
-			i--;
-		}
-		if(flag)
-		  break;
-		n-=2;
-	}
-	return n;
+	if(n<2)
+	  return 0;
+	for(int i = n-1;i>=2;i--)
+	  if(!(n%i))
+		return 0;
+	return 1;
 }
 unsigned long long get_key(string str)
 {
 	unsigned long long key=0;
 	for(int i = 0;i<str.length();i++){
-		key+=(str[i]);
 		key*=37;
+		key+=(str[i]);
 	}
 	return key;
 }
 void detect(bool*vec,unsigned long long key,unsigned long long len)
 {
 	int  i = 0;
-	int d = 1;
+	int d = 2;
+	//cout<<"key"<<key<<endl;
 	while(i<len){
+	//	cout<<(key+(d/2)*(d/2)+len)%len<<"&&&"<<(key-(d/2)*(d/2)+len)%len<<endl;
 		if(d%2){
-			if(vec[(key+(d/2)*(d/2))%len]){
-				vec[(key+(d/2)*(d/2))%len]=1;
-				break;
+			if(!vec[(key-(d/2)*(d/2)+len*len)%len]){
+				vec[(key-(d/2)*(d/2)+len*len)%len]=1;
+				cout<<(key-(d/2)*(d/2)+len*len)%len<<" ";
+				return;
 			}
 		}
 		else{
-			if(vec[(key-(d/2)*(d/2))%len]){
-				vec[(key-(d/2)*(d/2))%len]=1;
-				break;
+			if(!vec[(key+(d/2)*(d/2))%len]){
+				vec[(key+(d/2)*(d/2))%len]=1;
+				cout<<(key+(d/2)*(d/2))%len<<" ";
+				return;
 			}
 		}
 		d++;
 		i++;
 	}
-	if(i==len)
-	  cout<<"-"<<" ";
-	else 
-	  cout<<i<<" ";
+    cout<<"-"<<" ";
 	return ;
 }
 int main()
@@ -63,7 +52,9 @@ int main()
 	bool* vec;
 	int n,p;
 	cin>>n>>p;
-	p = get_max_prime(p);
+	while(!is_prime(p))
+	  p++;
+	//cout<<"p:"<<p<<endl;
 	string in;
 	vec = new bool[p];
 	for(int i = 0;i<p;i++)
@@ -72,10 +63,11 @@ int main()
 	for(int i = 0;i<n;i++){
 		cin>>in;
 		key = get_key(in);
+	//	cout<<key<<endl;
 		key%= p;
 		if(!vec[key]){
-			cout<<key<<" ";
 			vec[key]=1;
+			cout<<key<<" ";
 		}
 		else 
 		  detect(vec,key,p);
